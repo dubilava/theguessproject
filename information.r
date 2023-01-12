7539e2f5ec787c64f3773b4868110a7536cf8461
David Ubilava <david.ubilava@sydney.edu.au>
2023-01-12 13:30:09 +1100

    simplified the code

--- information.r
+++ information.r
@@ -3,7 +3,7 @@ library(data.table)
 library(ggplot2)
 
 # a function that simulates a time series with no structural change
-sim1 <- function(m,n){
+sim <- function(m,n,sc=FALSE){
   
   # the size of the first window and of the out-of-sample segment
   R <- round(.5*n)
@@ -12,46 +12,11 @@ sim1 <- function(m,n){
   set.seed(m)
   y <- rnorm(n)
   for(i in 2:n){
-    y[i] <- .7*y[i-1]+y[i]
-  }
-  
-  # store in a data.table
-  dt <- data.table(date=1:n,y=y)
-  
-  # lagged dependent variable and two forecast windows
-  dt[,`:=`(y1=shift(y,1),exp=as.numeric(NA),rol=as.numeric(NA))]
-  
-  for(i in 1:P){
-    # regressions
-    reg_exp <- lm(y~y1,data=dt[1:(R-1+i)])
-    reg_rol <- lm(y~y1,data=dt[i:(R-1+i)])
-    
-    # forecasts
-    dt$exp[R+i] <- reg_exp$coefficients["(Intercept)"]+reg_exp$coefficients["y1"]*dt$y1[R+i]
-    dt$rol[R+i] <- reg_rol$coefficients["(Intercept)"]+reg_rol$coefficients["y1"]*dt$y1[R+i]
-  }
-  
-  # forecast errors
-  dt[,`:=`(exp_e=y-exp,rol_e=y-rol)]
-  
-  rmsfe_exp <- sqrt(mean(dt$exp_e^2,na.rm=T))
-  rmsfe_rol <- sqrt(mean(dt$rol_e^2,na.rm=T))
-  
-  return(c(rmsfe_exp,rmsfe_rol))
-  
-}
-
-# a function that simulates a time series with structural change
-sim2 <- function(m,n){
-  
-  # the size of the first window and of the out-of-sample segment
-  R <- round(.5*n)
-  P <- n-R
-  
-  set.seed(m)
-  y <- rnorm(n)
-  for(i in 2:n){
-    y[i] <- .5*(1/n)*i+(.9-.4*(1/n)*i)*y[i-1]+y[i]
+    if(sc==FALSE){
+      y[i] <- .7*y[i-1]+y[i]
+    }else{
+      y[i] <- .5*(1/n)*i+(.9-.4*(1/n)*i)*y[i-1]+y[i]
+    }
   }
   
   # store in a data.table
@@ -87,13 +52,13 @@ n <- 480
 M <- 500
 
 # no structural change (will take a few minutes)
-rmsfe1_dt <- data.table(t(sapply(1:M,sim1,n=n)))
+rmsfe1_dt <- data.table(t(sapply(1:M,sim,n=n,sc=F)))
 colnames(rmsfe1_dt) <- c("exp","rol")
 
 rmsfe1_dt[,`:=`(dif=log(exp/rol))]
 
 # structural change (will take a few minutes)
-rmsfe2_dt <- data.table(t(sapply(1:M,sim2,n=n)))
+rmsfe2_dt <- data.table(t(sapply(1:M,sim,n=n,sc=T)))
 colnames(rmsfe2_dt) <- c("exp","rol")
 
 rmsfe2_dt[,`:=`(dif=log(exp/rol))]
